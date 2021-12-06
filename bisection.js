2002a2f8c8d1aab96dcce4392b70a7c50c1dbb1f
jsmojnu <learnwithmojnu@gmail.com>
2021-12-06 11:43:40 +0600

    ready to go

--- bisection.js
+++ bisection.js
@@ -1,12 +1,12 @@
-let epsilon = 0.01;
+const epsilon = 0.0005;
+let c = 0;
 function func(x) {
-    return x * x * x + (3 * x) - 5;
+    return x * x * x - (2 * x) - 5;
 }
 function bisection(a, b) {
     if (func(a) * func(b) >= 0) {
         return "Sorry, change the value of a and b";
     }
-    let c = a;
     while ((b - a) >= epsilon) {
         c = (a + b)/2;
         if (func(c) === 0.0) {
@@ -21,4 +21,4 @@ function bisection(a, b) {
     }
     return (c.toFixed(4));
 }
-console.log(bisection(1, 2));
+console.log(bisection(-1, 16));
